dc24dfb3d9aaa4738e2d9594e10045c47f8097d9
ryomaejii <82492270+ryomaejii@users.noreply.github.com>
2023-10-17 13:04:30 +0900

    feat: make title link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,15 @@
 import { ModeToggle } from '@/components/mode-toggle';
 import { PageSelect } from './page-select';
+import Link from 'next/link';
 
 export const Header = () => {
   return (
     <header className="flex items-center justify-between px-2 py-4 border-b border-b-border">
-      <h1 className="font-heading text-lg md:text-xl font-bold">
-        Hospital Smarter
-      </h1>
+      <Link href="/">
+        <h1 className="font-heading text-lg md:text-xl font-bold">
+          Hospital Smarter
+        </h1>
+      </Link>
       <div className="flex flex-0 gap-1">
         <ModeToggle />
         <PageSelect />
