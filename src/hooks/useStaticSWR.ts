bb42ce6ad58fd280c717e3c5a55c0fe3cfd70736
ryomaejii <82492270+ryomaejii@users.noreply.github.com>
2023-10-17 15:23:06 +0900

    feat: Implement useStaticSWR

---
+++ src/hooks/useStaticSWR.ts
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+import useSWR from 'swr';
+
+export const useStaticSWR = <T>({
+  key,
+  initialData,
+}: {
+  key: string;
+  initialData: T;
+}) => {
+  const { data = initialData, mutate } = useSWR<T>(key, null, {
+    fallbackData: initialData,
+    revalidateOnFocus: false, // 画面フォーカス時の再検証（apiリクエスト）をオフ
+    revalidateOnMount: false, // コンポーネントマウント時の再検証（apiリクエスト）をオフ
+    revalidateOnReconnect: false, // ブラウザがネットワーク接続できた時の再検証(apiリクエスト)をオフ
+    revalidateIfStale: false, // キャッシュが古くなったときの再検証（apiリクエスト）をオフ
+  });
+
+  useEffect(() => {
+    // swrでキャッシュされているdataがundefinedだった場合に、initialDataをset
+    void mutate((_data) => _data || initialData, false);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return { data, mutate };
+};
