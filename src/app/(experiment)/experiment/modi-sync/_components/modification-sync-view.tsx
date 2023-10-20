4427d60c907917544ce292edd63ac575356faa0a
ryomaejii <82492270+ryomaejii@users.noreply.github.com>
2023-10-20 15:23:23 +0900

    feat: Remove input with label and add removable input

--- src/app/(experiment)/experiment/modi-sync/_components/modification-sync-view.tsx
+++ src/app/(experiment)/experiment/modi-sync/_components/modification-sync-view.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { InputWithLabel } from '@/components/input-with-label';
 import { RecordingButton } from '@/components/recording-button';
 import { useASRInput } from '@/hooks/useASRInput';
 
@@ -12,13 +11,7 @@ export const ModificationSyncView = () => {
   return (
     <>
       <div>{recording ? transcript : value}</div>
-      <div className="space-y-8 mt-10">
-        <InputWithLabel id="体温" label="体温" />
-        <InputWithLabel id="脈拍毎分" label="脈拍毎分" />
-        <InputWithLabel id="呼吸毎分" label="呼吸毎分" />
-        <InputWithLabel id="血圧上" label="血圧上" />
-        <InputWithLabel id="血圧下" label="血圧下" />
-      </div>
+      <div className="space-y-8 mt-10"></div>
 
       <div className="fixed z-50 bottom-8 right-8">
         <RecordingButton onClick={toggleRecording} recording={recording} />
