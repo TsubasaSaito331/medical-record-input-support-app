c9a66664ec5ccc8e963c7df97c5ceb6c2842378f
ryomaejii <82492270+ryomaejii@users.noreply.github.com>
2023-10-20 15:23:36 +0900

    feat: Add rounds constants

---
+++ src/constants/rounds.ts
@@ -0,0 +1,34 @@
+export const ROUNDS_ITEMS = {
+  /* 体調 **/
+  TEMPERATURE: 'TEMPERATURE',
+  /* 脈拍 **/
+  PULSE: 'PULSE',
+  /* 血圧上 **/
+  BLOOD_PRESSURE_HIGH: 'BLOOD_PRESSURE_HIGH',
+  /* 血圧下 **/
+  BLOOD_PRESSURE_LOW: 'BLOOD_PRESSURE_LOW',
+  /* 呼吸回数 **/
+  RESPIRATION: 'RESPIRATION',
+  /** 補足事項 */
+  NOTE: 'NOTE',
+} as const;
+
+export type ROUNDS_ITEM = (typeof ROUNDS_ITEMS)[keyof typeof ROUNDS_ITEMS];
+
+export const ROUNDS_ITEM_LABELS = {
+  TEMPERATURE: '体温',
+  PULSE: '脈拍',
+  BLOOD_PRESSURE_HIGH: '血圧上',
+  BLOOD_PRESSURE_LOW: '血圧下',
+  RESPIRATION: '呼吸回数',
+  NOTE: '補足事項',
+} as const satisfies Record<ROUNDS_ITEM, string>;
+
+export const ROUNDS_ITEM_UNITS = {
+  TEMPERATURE: '度',
+  PULSE: '回/分',
+  BLOOD_PRESSURE_HIGH: 'mmHg',
+  BLOOD_PRESSURE_LOW: 'mmHg',
+  RESPIRATION: '回/分',
+  NOTE: '',
+} as const satisfies Record<ROUNDS_ITEM, string>;
