049bdda8ae93fb89cc5eedf0db65ab4481dcb06c
ryomaejii <82492270+ryomaejii@users.noreply.github.com>
2023-10-23 17:46:56 +0900

    style: Modify page title layout

--- src/components/page-title.tsx
+++ src/components/page-title.tsx
@@ -1,3 +1,5 @@
 export const PageTitle = ({ children }: { children: React.ReactNode }) => {
-  return <h2 className="text-2xl font-bold leading-9">{children}</h2>;
+  return (
+    <h2 className="text-xl font-bold leading-9 md:text-2xl">{children}</h2>
+  );
 };
