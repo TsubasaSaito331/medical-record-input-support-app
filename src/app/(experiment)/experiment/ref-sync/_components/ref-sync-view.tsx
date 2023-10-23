38b240de2474711e4a494d1e06befa4abbcf55fa
ryomaejii <82492270+ryomaejii@users.noreply.github.com>
2023-10-23 17:48:00 +0900

    feat: Handle storage time value

--- src/app/(experiment)/experiment/ref-sync/_components/ref-sync-view.tsx
+++ src/app/(experiment)/experiment/ref-sync/_components/ref-sync-view.tsx
@@ -1,10 +1,18 @@
 'use client';
 
+import { usePathname, useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+import { useSessionStorage } from 'react-use';
+
 import { RecordingButton } from '@/components/recording-button';
+import { Button } from '@/components/ui/button';
 import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
+import type { ExperimentEval } from '@/constants/experiment';
+import { EXPERIMENT_EVAL_KEY } from '@/constants/experiment';
 import { ROUNDS_ITEM_LABELS } from '@/constants/rounds';
 import { useRefSync } from '@/hooks/useRefSync';
+import { useTimer } from '@/hooks/useTimer';
 
 import { RefSyncInputs } from './ref-sync-inputs';
 
@@ -22,68 +30,108 @@ export const RefSyncView = () => {
     respirationPositions,
     notePositions,
   } = useRefSync();
+  const pathname = usePathname();
+  const router = useRouter();
+  const { startTimer, stopTimer, time } = useTimer();
+  const [refSyncEvalStorage, setRefSyncEvalStorage] =
+    useSessionStorage<ExperimentEval>(EXPERIMENT_EVAL_KEY.REF_SYNC);
+
+  const buttonDisabled =
+    temperaturePositions.length === 0 ||
+    pulsePositions.length === 0 ||
+    bloodPressureHighPositions.length === 0 ||
+    bloodPressureLowPositions.length === 0 ||
+    respirationPositions.length === 0 ||
+    notePositions.length === 0;
+
+  const onClickSendButton = () => {
+    stopTimer();
+    setRefSyncEvalStorage({
+      ...refSyncEvalStorage,
+      time: time,
+    });
+
+    router.push(pathname + '/eval');
+  };
+
+  // TODO: 開始前にダイアログを表示
+  useEffect(() => {
+    startTimer();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
-      {recording || originalValue.length === 0 ? (
-        <div>{originalValue + transcript}</div>
-      ) : (
-        <Textarea
-          value={originalValue}
-          onChange={(e) => setOriginalValue(e.target.value)}
-        />
-      )}
-      <div className="space-y-8 mt-10"></div>
-      <section className="space-y-8">
-        <div className="space-y-1">
-          <Label>{ROUNDS_ITEM_LABELS.TEMPERATURE}</Label>
-          <RefSyncInputs
-            originalText={originalValue}
-            pickUpPositions={temperaturePositions}
-            onChange={setOriginalValue}
+      <div className="space-y-8">
+        <p className="text-sm">所要時間: {Math.floor(time)}秒</p>
+        {recording || originalValue.length === 0 ? (
+          <div>{originalValue + transcript}</div>
+        ) : (
+          <Textarea
+            value={originalValue}
+            onChange={(e) => setOriginalValue(e.target.value)}
           />
+        )}
+        <section className="space-y-8">
+          <div className="space-y-1">
+            <Label>{ROUNDS_ITEM_LABELS.TEMPERATURE}</Label>
+            <RefSyncInputs
+              originalText={originalValue}
+              pickUpPositions={temperaturePositions}
+              onChange={setOriginalValue}
+            />
+          </div>
+          <div className="space-y-1">
+            <Label>{ROUNDS_ITEM_LABELS.PULSE}</Label>
+            <RefSyncInputs
+              originalText={originalValue}
+              pickUpPositions={pulsePositions}
+              onChange={setOriginalValue}
+            />
+          </div>
+          <div className="space-y-1">
+            <Label>{ROUNDS_ITEM_LABELS.BLOOD_PRESSURE_HIGH}</Label>
+            <RefSyncInputs
+              originalText={originalValue}
+              pickUpPositions={bloodPressureHighPositions}
+              onChange={setOriginalValue}
+            />
+          </div>
+          <div className="space-y-1">
+            <Label>{ROUNDS_ITEM_LABELS.BLOOD_PRESSURE_LOW}</Label>
+            <RefSyncInputs
+              originalText={originalValue}
+              pickUpPositions={bloodPressureLowPositions}
+              onChange={setOriginalValue}
+            />
+          </div>
+          <div className="space-y-1">
+            <Label>{ROUNDS_ITEM_LABELS.RESPIRATION}</Label>
+            <RefSyncInputs
+              originalText={originalValue}
+              pickUpPositions={respirationPositions}
+              onChange={setOriginalValue}
+            />
+          </div>
+          <div className="space-y-1">
+            <Label>{ROUNDS_ITEM_LABELS.NOTE}</Label>
+            <RefSyncInputs
+              originalText={originalValue}
+              pickUpPositions={notePositions}
+              onChange={setOriginalValue}
+            />
+          </div>
+        </section>
+        <div className="flex justify-center items-center">
+          <Button
+            size="lg"
+            disabled={buttonDisabled}
+            onClick={onClickSendButton}
+          >
+            送信
+          </Button>
         </div>
-        <div className="space-y-1">
-          <Label>{ROUNDS_ITEM_LABELS.PULSE}</Label>
-          <RefSyncInputs
-            originalText={originalValue}
-            pickUpPositions={pulsePositions}
-            onChange={setOriginalValue}
-          />
-        </div>
-        <div className="space-y-1">
-          <Label>{ROUNDS_ITEM_LABELS.BLOOD_PRESSURE_HIGH}</Label>
-          <RefSyncInputs
-            originalText={originalValue}
-            pickUpPositions={bloodPressureHighPositions}
-            onChange={setOriginalValue}
-          />
-        </div>
-        <div className="space-y-1">
-          <Label>{ROUNDS_ITEM_LABELS.BLOOD_PRESSURE_LOW}</Label>
-          <RefSyncInputs
-            originalText={originalValue}
-            pickUpPositions={bloodPressureLowPositions}
-            onChange={setOriginalValue}
-          />
-        </div>
-        <div className="space-y-1">
-          <Label>{ROUNDS_ITEM_LABELS.RESPIRATION}</Label>
-          <RefSyncInputs
-            originalText={originalValue}
-            pickUpPositions={respirationPositions}
-            onChange={setOriginalValue}
-          />
-        </div>
-        <div className="space-y-1">
-          <Label>{ROUNDS_ITEM_LABELS.NOTE}</Label>
-          <RefSyncInputs
-            originalText={originalValue}
-            pickUpPositions={notePositions}
-            onChange={setOriginalValue}
-          />
-        </div>
-      </section>
+      </div>
 
       <div className="fixed z-50 bottom-8 right-8">
         <RecordingButton onClick={toggleRecording} recording={recording} />
--- src/app/(experiment)/experiment/ref-sync/eval/_components/ref-sync-eval.tsx
+++ src/app/(experiment)/experiment/ref-sync/eval/_components/ref-sync-eval.tsx
@@ -6,19 +6,21 @@ import { useState } from 'react';
 import { useSessionStorage } from 'react-use';
 
 import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 import { Slider } from '@/components/ui/slider';
 import type { ExperimentEval } from '@/constants/experiment';
 import { EXPERIMENT_EVAL_KEY } from '@/constants/experiment';
 
 export const RefSyncEval = () => {
+  const [refSyncEvalStorage, setRefSyncEvalStorage] =
+    useSessionStorage<ExperimentEval>(EXPERIMENT_EVAL_KEY.REF_SYNC);
   const [evalValue, setEvalValue] = useState([2]);
+  const [time, setTime] = useState(refSyncEvalStorage.time);
   const router = useRouter();
-  const [, setRefSyncEvalStorage] = useSessionStorage<ExperimentEval>(
-    EXPERIMENT_EVAL_KEY.REF_SYNC,
-  );
 
   const onClickSendButton = () => {
     setRefSyncEvalStorage({
+      time: time,
       easeOfUse: evalValue[0] + 1, // 0 ~ 4 -> 1 ~ 5
     });
     router.push('/experiment');
@@ -26,7 +28,15 @@ export const RefSyncEval = () => {
 
   return (
     <div>
-      <div className="space-y-2">
+      <div className="space-y-1">
+        <Label>時間</Label>
+        <Input
+          value={time}
+          type="number"
+          onChange={(e) => setTime(Number(e.target.value))}
+        />
+      </div>
+      <div className="space-y-2 mt-8">
         <Label>使いやすさ</Label>
         <Slider
           value={evalValue}
@@ -35,7 +45,7 @@ export const RefSyncEval = () => {
           step={1}
         />
       </div>
-      <div className="flex justify-center mt-8">
+      <div className="flex justify-center mt-12">
         <Button onClick={onClickSendButton}>送信</Button>
       </div>
     </div>
