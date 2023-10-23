524787a9f83bc6810300cfaeab19346a4a359b89
ryomaejii <82492270+ryomaejii@users.noreply.github.com>
2023-10-23 14:11:41 +0900

    feat: add time to ExperimentalEval

--- src/constants/sessionStorage.ts
+++ src/constants/sessionStorage.ts
@@ -1,11 +1,12 @@
 export const EXPERIMENT_EVAL_KEY = {
   MODI_SYNC: 'EXPERIMENT_MODI_SYNC',
   REF_SYNC: 'EXPERIMENT_REF_SYNC',
-  USING_CHATGPT: 'EXPERIMENT_USING_CHATGPT',
+  USING_CHAT_GPT: 'EXPERIMENT_USING_CHAT_GPT',
 } as const;
 
 export const EXPERIMENT_EVAL_KEYS = Object.values(EXPERIMENT_EVAL_KEY);
 
 export interface ExperimentEval {
   easeOfUse: number;
+  time?: number;
 }
